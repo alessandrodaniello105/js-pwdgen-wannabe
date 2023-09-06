2addc5d784a0290da4253a3b306b53a751c4cafc
alessandrodaniello105 <kanymsevement@gmail.com>
2023-09-06 14:15:21 +0200

    [add] text trim to user data variables

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 const userName = prompt('Qual è il tuo nome?');
+const userNameRST = userName.trim();
 
 const userLastName = prompt('Qual è il tuo cognome?');
+const userLastNameRST = userLastName.trim();
 
 const userFavColor = prompt('Infine qual è il tuo colore preferito?');
+const userFavColorRST = userFavColor.trim();
 
 const magicNumber = 23;
 
-const semipwd = userName + userLastName + userFavColor + magicNumber;
-const pwdLowerCase = semipwd.toLowerCase();
+const pwdPre = userNameRST + userLastNameRST + userFavColorRST + magicNumber;
+const pwdLowerCase = pwdPre.toLowerCase();
 
 console.log(pwdLowerCase);
 
